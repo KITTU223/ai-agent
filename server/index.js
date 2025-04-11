e34f629397695f1e05dc2992a4034213ad7bb7b3
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2025-04-11 09:29:03 +0530

    formate change

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { z } from "zod";
 
 const server = new McpServer({
   name: "example-server",
-  version: "1.0.0"
+  version: "1.0.0",
 });
 
 // ... set up server resources, tools, and prompts ...
@@ -14,43 +14,43 @@ const server = new McpServer({
 const app = express();
 
 server.tool(
-  "addTwoNumbers", 
+  "addTwoNumbers",
   "Add two numbers",
   {
     a: z.number(),
-    b: z.number() 
+    b: z.number(),
   },
-  async(arg) => {
-    const { a,b } = arg;
-      return {
-        content: [
+  async (arg) => {
+    const { a, b } = arg;
+    return {
+      content: [
         {
           type: "text",
-          text:`The sum of ${a} and ${b} is ${a + b}`
-        }
-      ]
-    }
+          text: `The sum of ${a} and ${b} is ${a + b}`,
+        },
+      ],
+    };
   }
-)
+);
 
 server.tool(
   "createPost",
-  "Create a post on X formally known as Twitter", 
+  "Create a post on X formally known as Twitter",
   {
-    status: z.string(), 
+    status: z.string(),
   },
   async (arg) => {
     const { status } = arg;
     return createPost(status);
   }
-)
+);
 
 // to support multiple simultaneous connections we have a lookup object from
 // sessionId to transport
 const transports = {};
 
 app.get("/sse", async (req, res) => {
-  const transport = new SSEServerTransport('/messages', res);
+  const transport = new SSEServerTransport("/messages", res);
   transports[transport.sessionId] = transport;
   res.on("close", () => {
     delete transports[transport.sessionId];
@@ -59,15 +59,15 @@ app.get("/sse", async (req, res) => {
 });
 
 app.post("/messages", async (req, res) => {
-  const sessionId = req.query.sessionId ;
+  const sessionId = req.query.sessionId;
   const transport = transports[sessionId];
   if (transport) {
     await transport.handlePostMessage(req, res);
   } else {
-    res.status(400).send('No transport found for sessionId');
+    res.status(400).send("No transport found for sessionId");
   }
 });
 
-app.listen(3001,() => {
-    console.log("server running on http://localhost:3001");
+app.listen(3001, () => {
+  console.log("server running on http://localhost:3001");
 });
